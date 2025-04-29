46ae1393ec8e7734a7a32fac82ee00cb63bc961c
Krishna Singh <144099384+krishnasingh34@users.noreply.github.com>
2025-04-30 00:07:51 +0530

    sub

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 
-import React from "react";
+import React, { useContext } from "react";
 import './../styles/App.css';
+import { AuthContext } from "./AuthContext";
 
 const App = () => {
+  const {auth, setAuth} = useContext(AuthContext)
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Click on the checkbox to get authenticated</h1>
+      {
+        auth==true ? (
+          <p>You are now authenticated, you can proceed</p>
+        ) : (
+          <p>You are not authenticated</p>
+        )
+      }
+      <input type="checkbox" id="auth" onClick={()=> {
+        setAuth(true)
+      }}/>
+      <label> I'm not a robot </label>
     </div>
   )
 }
